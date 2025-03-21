21d0475afc4c83938b12fc221b21e340c152f736
Hasinikadali <kadalihasini1930@gmail.com>
2025-03-21 11:41:12 +0530

    commit

---
+++ src/App8.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import "./App8.css"
+export default function App8() {
+  return (
+    <div >
+      <div>
+        <div className="App"><p>React store</p>
+       <div> <a href="#">Home |</a>
+        <a href="#">Cart |</a>
+        <a href="#">Login </a>
+        </div>
+        </div>
+      <div>
+        <h3>Registration Form</h3>
+        <p><input type="name" placeholder="Enter name" /></p>
+        <p><input type="email" placeholder="Email address" /></p>
+        <p><input type="password" placeholder="New Password" /></p>
+        <p style={{}}><button>Submit</button></p>
+        <div> <a href="#" >Already have an Account? Log In</a></div>
+        <p>@Copyright 2025.All rights reserved.</p>
+      </div>
+    </div>
+    </div>
+  );
+}
